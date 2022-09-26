f0411fece302aabb53f7c5e2611558b42e5d891e
brunodono <bruno_bignard@hotmail.com>
2022-09-26 20:31:31 +0100

    first commit, banner and logo added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='banner-container'>
+        <img src='images/banner.png' alt='cabra pizza banner' />
+      </div>
+      <div className='logo-container'>
+        <img src='images/logo.png' alt='cabra pizza logo' />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
